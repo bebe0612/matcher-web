282ad88cc01b20cd7ebe15ff83cef35cca31b81f
darkdulgi <daniel99285@gmail.com>
2023-06-20 12:06:54 +0900

    feat : 게시판 조회 api 연동

--- pages/home/board/write.tsx
+++ pages/home/board/write.tsx
@@ -1,7 +1,10 @@
 import PostUpsertPage from "@/src/pages/post-upsert";
 import PageHead from "@/src/components/pagehead";
+import loginCheck from "@/src/components/loginCheck";
 
 export default function Index() {
+  loginCheck();
+  
   return (
     <>
       <PageHead title="글쓰기" />
