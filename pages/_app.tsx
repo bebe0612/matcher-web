3298b63fd0777c890e82fee8e495acb0e6e57f4a
darkdulgi <daniel99285@gmail.com>
2023-09-24 14:53:16 +0900

    fix : 서버 주소 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 
 export default function App({ Component, pageProps }: AppProps) {
   if (typeof window !== 'undefined') {
-    axios.defaults.baseURL = "http://ec2-13-124-183-78.ap-northeast-2.compute.amazonaws.com";
+    axios.defaults.baseURL = "http://ec2-13-125-198-121.ap-northeast-2.compute.amazonaws.com";
     axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem("gatherschool-token")}`;
   }
   return <Component {...pageProps} />;
